44235195a9dc79378a3f1c2d95c4ea65c37642a9
harshitaM29 <harshita.mundhe29@gmail.com>
2023-05-10 12:20:29 +0530

    Aysnc and await functionality

--- promise.js
+++ promise.js
@@ -1,36 +1,73 @@
 const posts =[{title: 'Post1'}];
-const users = [{name: 'Joe', lastActivity: '13th Jan'}]
+// const users = [{name: 'Joe', lastActivity: '13th Jan'}]
 
 
+function getPosts(){
+    setTimeout(() => {
+        posts.forEach((post, index) => {
+            console.log(post.title)
+        })
+    }, 1000);
+}
 function createPost(post) {
     return new Promise( (resolve, reject) => {
        
         setTimeout( () => {
-            posts.title = post;
-            resolve(posts.title);
+            posts.push(post)
+            resolve();
             
-        }, 1000)
+        }, 2000)
     }) 
 }
-function updateLastUserActivityTime()  {
-    return new Promise(( resolve, reject) => {
-        setTimeout(() => {
-            users.lastActivity = new Date().getTime();
-            resolve(users.lastActivity)
-        }, 1000)
-    })
+function deletePost() {
+    return new Promise( (resolve, reject) => {
+       
+        setTimeout( () => {
+           let poppedElement = posts.pop();
+            resolve(poppedElement);
+            
+        }, 2000)
+    }) 
 }
 
+async function start(){
+    await createPost({ title: 'Post2'});
+    
+    getPosts(); 
+    await deletePost()
+    getPosts(); 
+}
+
+start();
+// function createPost(post) {
+//     return new Promise( (resolve, reject) => {
+       
+//         setTimeout( () => {
+//             posts.title = post;
+//             resolve(posts.title);
+            
+//         }, 1000)
+//     }) 
+// }
+// function updateLastUserActivityTime()  {
+//     return new Promise(( resolve, reject) => {
+//         setTimeout(() => {
+//             users.lastActivity = new Date().getTime();
+//             resolve(users.lastActivity)
+//         }, 1000)
+//     })
+// }
 
 
-function userUpdatePost() {
-Promise.all([createPost({title:'Post3'}), updateLastUserActivityTime()]).then(([post,time]) => {
-    console.log(post)
-    console.log(time)
+
+// function userUpdatePost() {
+// Promise.all([createPost({title:'Post3'}), updateLastUserActivityTime()]).then(([post,time]) => {
+//     console.log(post)
+//     console.log(time)
    
-})
-.catch((err) =>{
-    console.log(err);
-}) 
-};
-userUpdatePost();
+// })
+// .catch((err) =>{
+//     console.log(err);
+// }) 
+// };
+// userUpdatePost();
